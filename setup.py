5df9c1f90c4d274b9c7a69c31894fa9f6e655813
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-03-14 12:52:51 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup 
+#from typing import List
+
+
+
+
+setup(
+    name="us_visa_approval",
+    version="0.0.0",
+    author="Anna_Elsa_Luiz",
+    author_email="luizannaelsa@gmail.com",
+    packages=find_packages()
+)  
+
+
