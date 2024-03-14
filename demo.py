6f0d4387135c1621e354fb39d4179d74526d387b
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-03-14 15:44:50 +0530

    exception file created

--- demo.py
+++ demo.py
@@ -1,3 +1,16 @@
+
+
 from us_visa_approval.logger import logging
+from us_visa_approval.exception import USvisaException
+import sys 
+
 
 logging.info("welcome to the custom log")
+
+try: 
+  a= 1/"10"
+
+except Exception as e:
+  logging.info(e)
+  raise USvisaException(e , sys) from  e 
+   
