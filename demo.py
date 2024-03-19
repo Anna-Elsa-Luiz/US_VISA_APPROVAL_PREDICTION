10c1162fb6420c62df09c893eed6bf114e9b7914
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-03-19 10:36:33 +0530

    checked the function

--- demo.py
+++ demo.py
@@ -1,16 +1,5 @@
 
 
-from us_visa_approval.logger import logging
-from us_visa_approval.exception import USvisaException
-import sys 
+from us_visa_approval.constants import * 
 
-
-logging.info("welcome to the custom log")
-
-try: 
-  a= 1/"10"
-
-except Exception as e:
-  logging.info(e)
-  raise USvisaException(e , sys) from  e 
-   
+print(COLLECTION_NAME)
