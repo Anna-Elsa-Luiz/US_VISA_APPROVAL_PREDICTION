07dda9e2eb677456d1e7f9fa8d4cbcbdbf19288b
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-03-19 16:08:03 +0530

    files updated

--- us_visa_approval/entity/artifact_entity.py
+++ us_visa_approval/entity/artifact_entity.py
@@ -3,5 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path:str 
-    test_file_path:str 
+    trained_file_path: str
+    test_file_path: str
