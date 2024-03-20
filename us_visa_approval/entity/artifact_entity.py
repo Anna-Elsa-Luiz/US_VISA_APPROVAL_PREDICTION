93a9c7bd2a103b99563e3376c18fcb15cf78791c
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-03-20 12:09:29 +0530

    data validation artifact updated

--- us_visa_approval/entity/artifact_entity.py
+++ us_visa_approval/entity/artifact_entity.py
@@ -3,5 +3,16 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionArtifact:
-    trained_file_path: str
-    test_file_path: str
+    trained_file_path:str 
+    test_file_path:str 
+
+
+
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    drift_report_file_path: str
+
+# it returns the error the data having --> message: str
+# return the report file path -->drift_report_file_path: str
