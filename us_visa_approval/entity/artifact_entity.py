4ce421e30c29253c95af48dce4ba2143a2319d84
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-03-20 13:18:24 +0530

    datatransformationartifact added

--- us_visa_approval/entity/artifact_entity.py
+++ us_visa_approval/entity/artifact_entity.py
@@ -16,3 +16,11 @@ class DataValidationArtifact:
 
 # it returns the error the data having --> message: str
 # return the report file path -->drift_report_file_path: str
+
+
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path:str 
+    transformed_train_file_path:str
+    transformed_test_file_path:str
